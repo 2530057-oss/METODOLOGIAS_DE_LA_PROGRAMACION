e1650a30b9177eb130ee1015978ed41e938b1ce1
valentin <2530057@upv.edu.mx>
2025-10-28 10:41:59 -0600

    primera_parte

---
+++ src/understanding_strings.py
@@ -0,0 +1,13 @@
+"""
+strings variables es de manera sencilla una serie 
+de caracteres. En python, todo lo que se encuentre
+ dentro de comillas simples (' ') o (" ")
+ sera considerado un stranding.
+
+ Ejemplos:
+    "Esto es un string"
+    'Esto tambien es un string' 
+
+' le dije a un amigo que "python es mi lenguaje favorito" '
+" El lenguaje 'pytho' lleva el nombre por monty python, no por la serpiente "
+"""
